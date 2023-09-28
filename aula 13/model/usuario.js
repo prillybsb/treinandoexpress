1208640962085eec39d1601486edb3a63f9b41b0
Priscila Lessa <prillybsb@gmail.com>
2023-09-27 21:16:15 -0300

    atualizando

---
+++ aula 13/model/usuario.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const UsuarioSchema = new mongoose.Schema({
+  nome: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  senha: { type: String, required: true },
+});
+
+const Usuario = mongoose.model("usuarios", UsuarioSchema);
+
+module.exports = Usuario;
