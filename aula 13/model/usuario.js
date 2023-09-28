f53b8447ab9f8e22ba6f2b5b3e0e502b4ff16399
Priscila Lessa <prillybsb@gmail.com>
2023-09-27 21:26:25 -0300

    login

--- aula 13/model/usuario.js
+++ aula 13/model/usuario.js
@@ -4,6 +4,7 @@ const UsuarioSchema = new mongoose.Schema({
   nome: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   senha: { type: String, required: true },
+  token: { type: String, required: true },
 });
 
 const Usuario = mongoose.model("usuarios", UsuarioSchema);
